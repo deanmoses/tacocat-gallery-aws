acbd6a7cd23906c26958a0ec2a9ea2d6579e31ea
moses <moses@tacocat.com>
2018-11-07 12:48:12 -0800

    File rename
    deployment_template.yaml is now stack.yaml

--- cloudformation/deploy_stack.sh
+++ cloudformation/deploy_stack.sh
@@ -11,21 +11,21 @@
 # state machine the same way it can reference external lambdas.  But until 
 # then we're using this python script.
 #
-inject_state_machine='python3 cloudformation/inject_state_machine_cfn.py -s state_machine/state_machine.json -c cloudformation/deployment_template.yaml -o cloudformation/deployment_template.provisional.yaml'
+inject_state_machine='python3 cloudformation/inject_state_machine_cfn.py -s state_machine/state_machine.json -c cloudformation/stack.yaml -o cloudformation/stack.provisional.yaml'
 
 #
 # Upload the local assets that the template references up to a bucket, and
 # generate a new template file whose Lambda CodeUri values point to the actual
 # S3 locations of the lambda source code
 #
-package='aws cloudformation package --template-file cloudformation/deployment_template.provisional.yaml --output-template-file cloudformation/deployment_template.final.yaml --s3-bucket cloudformationdeploysource'
+package='aws cloudformation package --template-file cloudformation/stack.provisional.yaml --output-template-file cloudformation/stack.final.yaml --s3-bucket cloudformationdeploysource'
 
 #
 # Deploy the stack
 #
 # Take the twice-processed template and use it to deploy the stack
 #
-deploy='aws cloudformation deploy --template-file cloudformation/deployment_template.final.yaml --stack-name TacocatGallery --capabilities CAPABILITY_IAM'
+deploy='aws cloudformation deploy --template-file cloudformation/stack.final.yaml --stack-name TacocatGallery --capabilities CAPABILITY_IAM'
 
 #
 # Execute each of the above commands, and go on to the next one if it succeeds
