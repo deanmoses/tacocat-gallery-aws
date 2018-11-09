e962711c885ed926cc30cbd5b65e9aa1d13c3c28
Dean Moses <moses@tacocat.com>
2018-11-09 11:52:55 -0800

    Storing parent album of image in DynamoDB

--- lambda_functions/store_image/index.js
+++ lambda_functions/store_image/index.js
@@ -1,5 +1,4 @@
 const getImageId = require("./image_id.js");
-const getAlbumId = require("./album_id.js");
 const createImage = require("./create_image.js");
 const updateImage = require("./update_image.js");
 const getS3ObjectMetadata = require("./get_s3_object_metadata.js");
@@ -18,16 +17,29 @@ const docClient = new AWS.DynamoDB.DocumentClient({
 /**
  * A Lambda function that creates/updates the image in DynamoDB.
  */
-exports.handler = async (event) => {
-    // console.log("Reading input from event:\n", util.inspect(event, {depth: 5}));
-    const imageId = getImageId(event.s3Key);
-    const albumId = getAlbumId(event.s3Key);
-	await createImage(docClient, tableName, imageId, albumId);
+exports.handler = async event => {
+	// console.log("Reading input from event:\n", util.inspect(event, {depth: 5}));
+	const imageId = getImageId(event.s3Key);
+	await createImage(docClient, tableName, imageId);
 
-	const s3ObjectMetadata = await getS3ObjectMetadata(s3, event.s3Bucket, event.s3Key);
-	const fileUploadTimeStamp = Math.floor(Date.parse(s3ObjectMetadata.LastModified) / 1000);
+	const s3ObjectMetadata = await getS3ObjectMetadata(
+		s3,
+		event.s3Bucket,
+		event.s3Key
+	);
+	const fileUploadTimeStamp = Math.floor(
+		Date.parse(s3ObjectMetadata.LastModified) / 1000
+	);
 	const thumbnailS3key = event.parallelResults[0];
 	const metadata = event.extractedMetadata;
 	const labels = event.parallelResults[0];
-	return await updateImage(docClient, tableName, imageId, fileUploadTimeStamp, thumbnailS3key, metadata, labels);
+	return await updateImage(
+		docClient,
+		tableName,
+		imageId,
+		fileUploadTimeStamp,
+		thumbnailS3key,
+		metadata,
+		labels
+	);
 };
