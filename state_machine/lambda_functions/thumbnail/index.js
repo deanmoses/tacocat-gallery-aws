7aac9f74d7677f0eb2d2457716c1b059966999ae
Dean Moses <moses@tacocat.com>
2019-02-12 21:32:13 -0800

    Thumbnail now looks like tacocat.com

--- state_machine/lambda_functions/thumbnail/index.js
+++ state_machine/lambda_functions/thumbnail/index.js
@@ -1,11 +1,6 @@
 // dependencies
-const S3 = require('aws-sdk/clients/s3');
-const gm = require('gm').subClass({imageMagick: true}); // Enable ImageMagick integration.
-const util = require('util');
-
-// constants
-const MAX_WIDTH = 250;
-const MAX_HEIGHT = 250;
+const S3 = require("aws-sdk/clients/s3");
+const gm = require("gm").subClass({ imageMagick: true }); // Enable ImageMagick integration.
 
 const s3 = new S3();
 
@@ -28,53 +23,62 @@ const s3 = new S3();
  * @param callback
  */
 exports.handler = (event, context, callback) => {
-    const s3Bucket = event.s3Bucket;
-    // Object key may have spaces or unicode non-ASCII characters.
-    const srcKey = decodeURIComponent(event.s3Key.replace(/\+/g, " "));
+	const s3Bucket = event.s3Bucket;
+	// Object key may have spaces or unicode non-ASCII characters.
+	const srcKey = decodeURIComponent(event.s3Key.replace(/\+/g, " "));
 
-    const getObjectPromise = s3.getObject({
-        Bucket: s3Bucket,
-        Key: srcKey
-    }).promise();
+	const getObjectPromise = s3
+		.getObject({
+			Bucket: s3Bucket,
+			Key: srcKey
+		})
+		.promise();
 
-    const size = event.extractedMetadata.dimensions;
-    const scalingFactor = Math.min(
-        MAX_WIDTH / size.width,
-        MAX_HEIGHT / size.height
-    );
-    const width = scalingFactor * size.width;
-    const height = scalingFactor * size.height;
+	var resizePromise = new Promise(resolve => {
+		getObjectPromise
+			.then(getObjectResponse => {
+				gm(getObjectResponse.Body)
+					.autoOrient()
+					.resize(200, 200 + "^") // resize, ^ means overflow to get dimensions
+					.gravity("Center") // set thumb cutting to center
+					.extent(200, 200) // draw white background, in case thumb doesn't fill image
+					.interlace("Line") // aka JPEG Progressive
+					.quality(85) // default is 75.  I'm seeing smaller files at 85!
+					.noProfile() // remove EXIF, ICM, etc profile data
+					.toBuffer(event.extractedMetadata.format, (err, buffer) => {
+						if (err) {
+							callback(err);
+						} else {
+							resolve(buffer);
+						}
+					});
+			})
+			.catch(function(err) {
+				callback(err);
+			});
+	});
 
-    var resizePromise = new Promise((resolve) => {
-        getObjectPromise.then((getObjectResponse) => {
-            gm(getObjectResponse.Body).resize(width, height).toBuffer(event.extractedMetadata.format, (err, buffer) => {
-                if (err) {
-                    callback(err);
-                } else {
-                    resolve(buffer);
-                }
-            });
-        }).catch(function (err) {
-            callback(err);
-        });
-    })
+	const destKey = "Thumbnail/" + event.objectID;
 
-    const destKey = "Thumbnail/" + event.objectID;
+	const s3PutParams = {
+		Bucket: s3Bucket,
+		Key: destKey,
+		ContentType: "image/" + event.extractedMetadata.format.toLowerCase()
+	};
 
-    const s3PutParams = {
-        Bucket: s3Bucket,
-        Key: destKey,
-        ContentType: "image/" + event.extractedMetadata.format.toLowerCase()
-    };
-
-    resizePromise.then(function (buffer) {
-        s3PutParams.Body = buffer;
-        s3.upload(s3PutParams).promise().then((data) => {
-            callback(null, {"s3Bucket": s3Bucket, "s3Key": destKey});
-        }).catch(function (err) {
-            callback(err);
-        })
-    }).catch(function (err) {
-        callback(err);
-    })
-}
+	resizePromise
+		.then(function(buffer) {
+			s3PutParams.Body = buffer;
+			s3.upload(s3PutParams)
+				.promise()
+				.then(data => {
+					callback(null, { s3Bucket: s3Bucket, s3Key: destKey });
+				})
+				.catch(function(err) {
+					callback(err);
+				});
+		})
+		.catch(function(err) {
+			callback(err);
+		});
+};
