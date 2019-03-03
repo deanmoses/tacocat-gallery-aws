33c4fe67ec36e924eba179facda4c71e36e53b57
Dean Moses <moses@tacocat.com>
2019-03-02 20:03:57 -0800

    Run Jest tests faster

--- jest.config.js
+++ jest.config.js
@@ -1,2 +1,4 @@
 // Configuration for Jest unit tests (integration test config inherits from this)
-module.exports = {};
+module.exports = {
+	testEnvironment: "node" // makes tests run faster by disabling JSDom.  See https://medium.com/@kevinsimper/how-to-disable-jsdom-in-jest-make-jest-run-twice-as-fast-a01193f23405
+};
