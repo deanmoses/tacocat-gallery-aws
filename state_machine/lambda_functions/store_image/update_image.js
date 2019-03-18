0600f26631941506f8cbc6b69504a6e470481eff
Dean Moses <moses@tacocat.com>
2019-03-17 20:55:02 -0700

    Update image in single DynamoDB call #2
    I missed checking in the main file!
    
    Instead of doing a separate putItem to put the createdOn, do it in single updateItem using DynamoDB's if_not_exists() function that I just discovered.

--- state_machine/lambda_functions/store_image/update_image.js
+++ state_machine/lambda_functions/store_image/update_image.js
@@ -2,47 +2,43 @@ const { getParentAndNameFromPath } = require("gallery-path-utils");
 const { DynamoUpdateBuilder } = require("dynamo-utils");
 
 /**
- * Update image in DynamoDB.
+ * Update an already-existing image in DynamoDB.
  *
  * To be called every time a new version of the image is uploaded to S3.
  *
  * @param {Object} ctx execution context
  * @param {String} imagePath Path of the image like /2001/12-31/image.jpg
  * @param {Object} metadata EXIF and IPTC metadata extracted from the image
+ * @param {String} imageUpdatedOn ISO 8601 timestamp of when image was last updated
  *
- * @returns {Object} result from DynamoDB if success, throws exception if there's a problem with the input
+ * @returns {Object} DynamoDB update command
  */
-async function updateImage(ctx, imagePath, metadata) {
+async function updateImage(ctx, imagePath, metadata, imageUpdatedOn) {
+	const pathParts = getParentAndNameFromPath(imagePath);
 	const now = new Date().toISOString();
 
 	const bldr = new DynamoUpdateBuilder();
+	bldr.setIfNotExists("itemType", "media");
+	bldr.setIfNotExists("createdOn", now);
+	bldr.setIfNotExists("title", metadata.title);
+	bldr.setIfNotExists("description", metadata.description);
 	bldr.add("updatedOn", now);
-	bldr.add("fileUpdatedOn", now);
-	bldr.add("mimeSubType", metadata.format.toLowerCase());
-	bldr.add("dimensions", metadata.dimensions);
-	bldr.add("capturedOn", metadata.creationTime); // Don't check the date format: that was done in the transform metadata step
-	bldr.add("tags", metadata.tags);
+	bldr.add("fileUpdatedOn", imageUpdatedOn);
+	bldr.add("mimeSubType", metadata.format.toLowerCase()); // Don't validate metadata: already done in the transform metadata step
+	bldr.add("dimensions", metadata.dimensions); // Don't validate metadata: already done in the transform metadata step
+	bldr.add("capturedOn", metadata.creationTime); // Don't validate metadata: already done in the transform metadata step
+	bldr.add("tags", metadata.tags); // Don't validate metadata: already done in the transform metadata step
 
-	//
-	// Construct the DynamoDB update statement
-	//
-
-	const pathParts = getParentAndNameFromPath(imagePath);
-	const ddbparams = {
+	const dynamoParams = {
 		TableName: ctx.tableName,
 		Key: {
 			parentPath: pathParts.parent,
 			itemName: pathParts.name
 		},
 		UpdateExpression: bldr.getUpdateExpression(),
-		ExpressionAttributeValues: bldr.getExpressionAttributeValues(),
-		ConditionExpression: "attribute_exists (itemName)"
+		ExpressionAttributeValues: bldr.getExpressionAttributeValues()
 	};
 
-	//
-	// Send update to DynamoDB
-	//
-
-	await ctx.doUpdate(ddbparams);
+	await ctx.doUpdate(dynamoParams);
 }
 module.exports = updateImage;
