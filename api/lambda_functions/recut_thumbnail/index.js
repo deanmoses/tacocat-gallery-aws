981932f193abcd1f12139faadbf54ffbf8d6eb66
Dean Moses <moses@tacocat.com>
2019-03-19 21:37:10 -0700

    Recut Thumbnail now updates the image timestamp on any album for which it is the thumbnail
    Also, refactored the entire module to make use of the latest best practices

--- api/lambda_functions/recut_thumbnail/index.js
+++ api/lambda_functions/recut_thumbnail/index.js
@@ -1,151 +1,67 @@
 const recutThumbnail = require("./recut_thumbnail.js");
-const saveThumbnailCropInfoToDynamo = require("./save_thumb_crop_info_to_dynamo.js");
-const {
-	NotFoundException,
-	BadRequestException
-} = require("http-response-utils");
+const generateThumbnail = require("./generate_thumbnail.js");
+const { handleHttpExceptions } = require("http-response-utils");
 const AWS = require("aws-sdk");
-const s3 = new AWS.S3();
 
+const s3 = new AWS.S3();
 const s3BucketName = process.env.ORIGINAL_IMAGE_BUCKET; // name of the S3 containing original image
 const originalImagePrefix = process.env.ORIGINAL_IMAGE_S3_PREFIX; // S3 key prefix under which to read original image
 const derivedImageBucketName = process.env.DERIVED_IMAGE_BUCKET; // name of S3 bucket in which to store resized image
 const thumbnailImagePrefix = process.env.THUMBNAIL_IMAGE_S3_PREFIX; // S3 key prefix under which to store resized image
 const edgeSize = process.env.THUMBNAIL_IMAGE_SIZE; // longest edge of the resized image, in pixels
 const jpegQuality = process.env.THUMBNAIL_IMAGE_QUALITY; // JPEG quality of the resized image
+const tableName = process.env.GALLERY_ITEM_DDB_TABLE; // DyanmoDB table containing albums and images
 
-const dynamoTableName = process.env.GALLERY_ITEM_DDB_TABLE;
 const dynamoDocClient = new AWS.DynamoDB.DocumentClient({
 	region: process.env.AWS_REGION
 });
 
 /**
- * Generate a thumbnail of an image stored in s3 and store the thumbnail back in the same bucket under the "Thumbnail/" prefix.
+ * Generate a thumbnail of an image stored in s3 and
+ * store the thumbnail back in the same bucket
+ * under the "Thumbnail/" prefix.
  *
- * @param event an event object coming from AWS API Gateway. It should have the following format:
- *          {
- *                "s3Bucket": "mybucket",
- *                "s3Key": "mykey",
- *                "objectID": "l3j4234-234",
- *                "extractedMetadata": {
- *                   "dimensions": {
- *                      "width": 4567,
- *                      "height": 3456
- *                   }
- *                }
- *          }
- *
- * @returns something like the following format, as required by AWS API Gateway:
- * 		{
- * 			isBase64Encoded: false,
- *			statusCode: 400,
- *			body: JSON.stringify({ errorMessage: "Some error message" })
- * 		}
+ * @param event an event coming from AWS API Gateway
  */
 exports.handler = async event => {
-	const ctx = {
-		s3: s3,
-		s3BucketName: s3BucketName,
-		originalImagePrefix: originalImagePrefix,
-		derivedImageBucketName: derivedImageBucketName,
-		thumbnailImagePrefix: thumbnailImagePrefix,
-		edgeSize: edgeSize,
-		jpegQuality: jpegQuality,
-		event: event
+	//
+	// Set up execution context
+	//
+	const ctx = {};
+	ctx.tableName = tableName;
+	ctx.doSaveThumbCrop = async dynamoParams => {
+		return dynamoDocClient.update(dynamoParams).promise();
 	};
+	ctx.doUpdateAlbumThumb = async dynamoParams => {
+		return dynamoDocClient.batchWrite(dynamoParams).promise();
+	};
+	ctx.generateThumb = async (imagePath, crop) => {
+		if (!s3BucketName) throw "Undefined s3BucketName";
+		if (!originalImagePrefix) throw "Undefined originalImagePrefix";
+		if (!derivedImageBucketName) throw "Undefined derivedImageBucketName";
+		if (!thumbnailImagePrefix) throw "Undefined thumbnailImagePrefix";
+		if (!edgeSize) throw "Undefined edgeSize";
+		if (!jpegQuality) throw "Undefined jpegQuality";
 
-	return await doIt(ctx);
-};
-
-async function doIt(ctx) {
-	if (!ctx) throw "Undefined ctx";
-	if (!ctx.s3BucketName) throw "Undefined s3BucketName";
-	if (!ctx.originalImagePrefix) throw "Undefined originalImagePrefix";
-	if (!ctx.derivedImageBucketName) throw "Undefined derivedImageBucketName";
-	if (!ctx.thumbnailImagePrefix) throw "Undefined thumbnailImagePrefix";
-	if (!ctx.edgeSize) throw "Undefined edgeSize";
-	if (!ctx.jpegQuality) throw "Undefined jpegQuality";
-	if (!ctx.event) throw "Undefined event";
-
-	// event.path is passed in from the API Gateway and contains the full path
-	// of the HTTP request, such as  "/thumb/2001/12-31/image.jpg"
-	if (!ctx.event.path) {
-		return {
-			isBase64Encoded: false,
-			statusCode: 400,
-			body: JSON.stringify({ errorMessage: "HTTP path cannot be empty" })
-		};
-	}
-
-	// Remove the first segment of the URL path to get the image path
-	const imagePath = ctx.event.path.replace("/thumb", "");
-
-	// event.body contains the body of the HTTP request
-	if (!ctx.event.body) {
-		return {
-			isBase64Encoded: false,
-			statusCode: 400,
-			body: JSON.stringify({ errorMessage: "HTTP body cannot be empty" })
-		};
-	}
-
-	// Turn the body into a javascript object
-	let crop = JSON.parse(ctx.event.body);
-
-	try {
-		// Cut the new thumbnail
-		crop = await recutThumbnail(
-			ctx.s3,
-			ctx.s3BucketName,
-			ctx.originalImagePrefix,
-			ctx.derivedImageBucketName,
-			ctx.thumbnailImagePrefix,
-			ctx.edgeSize,
-			ctx.jpegQuality,
+		return await generateThumbnail(
+			s3,
+			s3BucketName,
+			originalImagePrefix,
+			derivedImageBucketName,
+			thumbnailImagePrefix,
+			edgeSize,
+			jpegQuality,
 			imagePath,
 			crop
 		);
+	};
 
-		// Set up execution context for saveThumbnailCropInfoToDynamo()
-		// This is everything the method needs in order to execute
-		// This is done to make the method unit testable
-		ctx = {};
-		ctx.tableName = dynamoTableName;
-		ctx.doUpdate = async dynamoParams => {
-			return dynamoDocClient.update(dynamoParams).promise();
-		};
-
-		// Save the crop to Dynamo
-		await saveThumbnailCropInfoToDynamo(ctx, imagePath, crop);
-
-		// Success
-		return {
-			isBase64Encoded: false,
-			statusCode: 200,
-			body: JSON.stringify({
-				successMessage: "Recut thumbnail of image " + imagePath
-			})
-		};
+	//
+	// Do the lambda's work
+	//
+	try {
+		return await recutThumbnail(event, ctx);
 	} catch (e) {
-		if (e instanceof NotFoundException) {
-			return {
-				statusCode: 404,
-				body: JSON.stringify({ errorMessage: e.message }),
-				isBase64Encoded: false
-			};
-		} else if (e instanceof BadRequestException) {
-			return {
-				statusCode: 400,
-				body: JSON.stringify({ errorMessage: e.message }),
-				isBase64Encoded: false
-			};
-		} else {
-			return {
-				statusCode: 500,
-				body: JSON.stringify({ errorMessage: e }), // TODO: handle case where e is not a string
-				isBase64Encoded: false
-			};
-		}
+		return handleHttpExceptions(e);
 	}
-}
-exports.doIt = doIt;
+};
--- api/lambda_functions/recut_thumbnail/recut_thumbnail.js
+++ api/lambda_functions/recut_thumbnail/recut_thumbnail.js
@@ -1,35 +1,32 @@
 const { BadRequestException } = require("http-response-utils");
 const { NotFoundException } = require("http-response-utils");
-const gm = require("gm").subClass({ imageMagick: true }); // Enable ImageMagick integration
+const { respondHttp } = require("http-response-utils");
+const { getParentAndNameFromPath } = require("gallery-path-utils");
+const { DynamoUpdateBuilder } = require("dynamo-utils");
 
 /**
- * Generate a thumbnail of a JPEG image stored in an AWS S3 bucket and save
- * the thumbnail back in the same S3 bucket.
+ * Generate a thumbnail of an image stored in s3 and
+ * store the thumbnail back in the same bucket under the "Thumbnail/" prefix.
  *
- * @param {*} s3 AWS S3 client
- * @param {*} s3BucketName name of the S3 bucket in which the original images are are stored
- * @param {*} originalImagePrefix the S3 prefix of the original images, such as "albums"
- * @param {*} derivedImageBucketName name of S3 bucket in which to store resized image
- * @param {*} thumbnailImagePrefix the S3 prefix of the thumbnail images, such as "thumbnails"
- * @param {*} edgeSize longest edge of the thumbnail image, in pixels
- * @param {*} jpegQuality JPEG quality of the resized image
- * @param {*} imagePath path of image like /2001/12-31/image.jpg
- * @param {*} crop thumbnail crop info in the format {x:INTEGER,y:INTEGER,length:INTEGER}
- *
- * @returns {} nothing if success
- * @throws exception if there's any problem
+ * @param {Object} event an event object coming from AWS API Gateway
+ * @param {Object} ctx the environmental context needed to do the work
  */
-async function recutThumbnail(
-	s3,
-	s3BucketName,
-	originalImagePrefix,
-	derivedImageBucketName,
-	thumbnailImagePrefix,
-	edgeSize,
-	jpegQuality,
-	imagePath,
-	crop
-) {
+async function recutThumbnail(event, ctx) {
+	if (!ctx) throw "Undefined ctx";
+
+	// event.path is passed in from the API Gateway and contains the full path
+	// of the HTTP request, such as  "/thumb/2001/12-31/image.jpg"
+	if (!event.path) throw new BadRequestException("HTTP path cannot be empty");
+
+	// event.body contains the body of the HTTP request
+	if (!event.body) throw new BadRequestException("HTTP body cannot be empty");
+
+	// Remove the first segment of the URL path to get the image path
+	const imagePath = event.path.replace("/thumb", "");
+
+	// Turn the body into a javascript object
+	let crop = JSON.parse(event.body);
+
 	if (!crop) {
 		throw new BadRequestException("Crop not specified");
 	}
@@ -46,54 +43,42 @@ async function recutThumbnail(
 		length: Number(crop.length)
 	};
 
-	// Get the original image
-	let s3Object;
-	try {
-		s3Object = await s3
-			.getObject({
-				Bucket: s3BucketName,
-				Key: originalImagePrefix + imagePath
-			})
-			.promise();
-	} catch (e) {
-		if (e.code === "AccessDenied") {
-			throw new NotFoundException("No such image: " + imagePath);
-		} else {
-			throw e;
+	// Cut the new thumbnail
+	await ctx.generateThumb(imagePath, crop);
+
+	// Set timestamp of the crop
+	const thumbUpdatedOn = new Date().toISOString();
+	crop.fileUpdatedOn = thumbUpdatedOn;
+
+	// Save the crop to DynamoDB
+	const updateResult = await saveThumbnailCropInfoToDynamo(
+		ctx,
+		imagePath,
+		crop
+	);
+
+	// For every album for which the image is the thumb,
+	// update the album’s thumbnail.fileUpdatedOn
+	if (updateResult.thumbForAlbums.length > 0) {
+		await updateAlbumThumbs(
+			ctx,
+			updateResult.thumbForAlbums,
+			imagePath,
+			thumbUpdatedOn
+		);
+	}
+
+	// Build success message
+	let msg = "Recut thumbnail of image " + imagePath;
+	if (updateResult.thumbForAlbums.length > 0) {
+		msg += ". Updated thumb info on these albums:";
+		for (const albumPath of updateResult.thumbForAlbums) {
+			msg += " " + albumPath;
 		}
 	}
 
-	// Cut the thumbnail
-	const buffer = await new Promise((resolve, reject) => {
-		gm(s3Object.Body)
-			.autoOrient()
-			.crop(crop.length, crop.length, crop.x, crop.y)
-			.resize(edgeSize, edgeSize + "^") // resize, ^ means overflow to get dimensions (shouldn't need it because I just cropped it to square)
-			.interlace("Line") // aka JPEG Progressive
-			.quality(jpegQuality)
-			.noProfile() // remove EXIF, ICM, etc profile data // TODO: add copyright info
-			.toBuffer("jpg", (err, buffer) => {
-				if (err) {
-					return reject(err);
-				}
-				resolve(buffer);
-			});
-	});
-
-	// Write the thumbnail to S3
-	await s3
-		.upload({
-			Bucket: derivedImageBucketName,
-			Key: thumbnailImagePrefix + imagePath,
-			ContentType: "image/jpeg",
-			Body: buffer
-		})
-		.promise();
-
-	// Return the cleaned
-	return crop;
+	return respondHttp({ successMessage: msg });
 }
-
 module.exports = recutThumbnail;
 
 /**
@@ -119,4 +104,143 @@ function isInt(x) {
 		return true;
 
 	return false;
+}
+
+/**
+ * Save image thumbnail crop info to DynamoDB
+ *
+ * @param {Object} ctx execution context
+ * @param {String} imagePath Path of the image to update, like /2001/12-31/image.jpg
+ * @param {Object} crop thumbnail crop info in the format {x:INTEGER,y:INTEGER,length:INTEGER}
+ *
+ * @returns {Object} empty {} object if success
+ * @throws {Object} the albums that this image is the thumbnail for, like:
+ *   {
+ * 	    thumbForAlbums: ["/2001/12-31/"],
+ * 	    albumCount: 1
+ *   }
+ */
+async function saveThumbnailCropInfoToDynamo(ctx, imagePath, crop) {
+	const bldr = new DynamoUpdateBuilder();
+	bldr.add("updatedOn", new Date().toISOString());
+	bldr.add("thumbnail", crop);
+
+	const pathParts = getParentAndNameFromPath(imagePath);
+
+	const dynamoParams = {
+		TableName: ctx.tableName,
+		Key: {
+			parentPath: pathParts.parent,
+			itemName: pathParts.name
+		},
+		UpdateExpression: bldr.getUpdateExpression(),
+		ExpressionAttributeValues: bldr.getExpressionAttributeValues(),
+		ConditionExpression: "attribute_exists (itemName)",
+		ReturnValues: "ALL_NEW"
+	};
+
+	let updatedImage;
+	try {
+		updatedImage = await ctx.doSaveThumbCrop(dynamoParams);
+	} catch (e) {
+		if (e.toString().indexOf("conditional") !== -1) {
+			throw new NotFoundException("Image not found: " + imagePath);
+		} else {
+			throw e;
+		}
+	}
+
+	//
+	// Extract the albums that the image is the thumb on, if any
+	//
+	// This operates over a result from DynamoDB that looks like this:
+	// {
+	//   "Attributes": {
+	// 		...
+	//   	"thumbForAlbums": {
+	// 			"wrapperName": "Set",
+	// 			"values": [
+	// 	  			"/2018/01-24/"
+	// 			],
+	// 			"type": "String"
+	//   	},
+	//   ...
+	// 	 }
+	// }
+	//
+
+	const result = {};
+	const thumbForAlbums = updatedImage.Attributes.thumbForAlbums;
+	if (thumbForAlbums !== undefined) {
+		result.thumbForAlbums = thumbForAlbums.values;
+		result.albumCount = thumbForAlbums.values.length;
+	} else {
+		result.thumbForAlbums = [];
+		result.albumCount = 0;
+	}
+	return result;
+}
+
+/**
+ * For every album for which the image is the thumb, update the album’s
+ * thumbnail.fileUpdatedOn
+ *
+ * @param {Object} ctx the environmental context needed to do the work
+ * @param {Array} thumbForAlbums array of albums like ["/2001/12-31/"]
+ */
+async function updateAlbumThumbs(
+	ctx,
+	thumbForAlbums,
+	imagePath,
+	thumbUpdatedOn
+) {
+	if (!imagePath) throw "Undefined imagePath";
+	if (!thumbUpdatedOn) throw "Undefined thumbUpdatedOn";
+	const now = new Date().toISOString();
+
+	// TODO: we don't want to loop through DynamoDB calls:
+	// 1) If there's a lot of albums to loop through, we'll time out
+	// 2) If there's an error, there's no retry built in
+	// Unfortuately there's no batch call for updates; batch is just for puts and deletes.
+	// And we don't want a a transaction: we don't want all of them to fail
+	// if one of them fails.
+	// So probably the best bet is to make a StepFunction that can
+	// retry each one individually if it fails.
+
+	// For every album for which the image is the thumb,
+	// update the album' thumbnail.fileUpdatedOn
+	for (const albumPath of thumbForAlbums) {
+		const bldr = new DynamoUpdateBuilder();
+		bldr.add("updatedOn", now);
+		bldr.addAlias("thumbnail.fileUpdatedOn", "thumbUpdatedOn", thumbUpdatedOn);
+		bldr.addValue("imagePath", imagePath);
+
+		const pathParts = getParentAndNameFromPath(albumPath);
+
+		const dynamoParams = {
+			TableName: ctx.tableName,
+			Key: {
+				parentPath: pathParts.parent,
+				itemName: pathParts.name
+			},
+			UpdateExpression: bldr.getUpdateExpression(),
+			ExpressionAttributeValues: bldr.getExpressionAttributeValues(),
+			ConditionExpression:
+				"attribute_exists (itemName) and thumbnail.path = :imagePath"
+		};
+
+		// Do the update
+		try {
+			await ctx.doUpdateAlbumThumb(dynamoParams);
+		} catch (e) {
+			// If the condition isn't met, it's either because the album
+			// doesn't exist or the image is no longer its thumbnail.  In
+			// either case, it's not an error.
+			if (!e.toString().includes("conditional")) {
+				// If it *is* an error, do we really want to stop execution of the others? No!
+				// But we want this here for debuggability
+				throw e;
+			}
+		}
+	}
 }
